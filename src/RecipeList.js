28f299aa037ef0e21bbb92a03aaa6ac56af24962
weldish <sc19ws2@leeds.ac.uk>
2024-04-10 12:17:04 +0100

    now accepts the setselectedrecipe method as a prop from the app componnent

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 // this componenet recievies a list of recipes and dispalys them
 
-function RecipeList({ recipes }) {
+function RecipeList({ recipes, setSelectedRecipe }) {
   return (
     <div>
       {recipes.map((recipe) => (
-        <div key={recipe.id} className="recipe-item">
+        <div key={recipe.id} className="recipe-item" onClick={() => setSelectedRecipe(recipe)}>
           <h2>{recipe.title}</h2>
           <img src={recipe.image} alt={recipe.title} style={{ width: '100px' }} />
         </div>
